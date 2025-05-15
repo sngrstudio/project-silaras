78e84fab3515dced5aca9d69191a2c968e423cec
R. Ilham Sastronegoro <bohrium.bearded_64@icloud.com>
2025-05-15 18:11:30 +0700

    Pending changes exported from your codespace (#2)

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 import settings from './settings'
 import user from './user'
 import image from './image'
+import region from './region'
 
-export const server = { settings, user, image }
+export const server = { settings, user, image, region }
--- src/actions/settings.ts
+++ src/actions/settings.ts
@@ -1,4 +1,4 @@
-import { defineAction, ActionError } from 'astro:actions'
+import { defineAction } from 'astro:actions'
 import { getSettings, updateSettings, getMenu } from '~/db/queries/settings'
 import { deletePresignedImage } from '~/db/queries/image'
 import { s3 } from '~/lib/s3'
@@ -8,15 +8,7 @@ import { z } from 'astro:schema'
 const settings = {
   get: defineAction({
     handler: async () => {
-      try {
-        return await getSettings()
-      } catch (error) {
-        console.log(error)
-        throw new ActionError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: 'Terjadi masalah di server kami.'
-        })
-      }
+      return await getSettings()
     }
   }),
 
@@ -28,37 +20,27 @@ const settings = {
       logo: z.instanceof(File).optional()
     }),
     handler: async ({ logo: logoFile, ...input }) => {
-      try {
-        const existingSettings = await getSettings()
-        let logo = undefined
-        if (logoFile && logoFile.name) {
-          const existingLogo = existingSettings.find(
-            (p) => p.property === 'SITE_LOGO'
-          )
-          if (existingLogo && existingLogo.value) {
-            const exFile = s3.file(existingLogo.value)
-            await deletePresignedImage(existingLogo.value)
-            await exFile.delete()
-          }
-
-          const fileExt = logoFile.name.substring(
-            logoFile.name.lastIndexOf('.')
-          )
-          const fileName = `logo-${Bun.randomUUIDv7()}${fileExt}`
-          const file = s3.file(fileName)
-
-          await write(file, logoFile)
-          logo = fileName
+      const existingSettings = await getSettings()
+      let logo = undefined
+      if (logoFile && logoFile.name) {
+        const existingLogo = existingSettings.find(
+          (p) => p.property === 'SITE_LOGO'
+        )
+        if (existingLogo && existingLogo.value) {
+          const exFile = s3.file(existingLogo.value)
+          await deletePresignedImage(existingLogo.value)
+          await exFile.delete()
         }
 
-        return await updateSettings({ logo, ...input })
-      } catch (error) {
-        console.log(error)
-        throw new ActionError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: 'Terjadi masalah di server kami.'
-        })
+        const fileExt = logoFile.name.substring(logoFile.name.lastIndexOf('.'))
+        const fileName = `logo-${Bun.randomUUIDv7()}${fileExt}`
+        const file = s3.file(fileName)
+
+        await write(file, logoFile)
+        logo = fileName
       }
+
+      return await updateSettings({ logo, ...input })
     }
   }),
 
@@ -66,15 +48,7 @@ const settings = {
   menu: {
     get: defineAction({
       handler: async () => {
-        try {
-          return await getMenu()
-        } catch (error) {
-          console.log(error)
-          throw new ActionError({
-            code: 'INTERNAL_SERVER_ERROR',
-            message: 'Terjadi masalah di server kami.'
-          })
-        }
+        return await getMenu()
       }
     })
   }
