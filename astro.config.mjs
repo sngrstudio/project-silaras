ddde79100ae83115015f3318a4418402a20e4cbc
R. Ilham Sastronegoro <bohrium.bearded_64@icloud.com>
2025-06-03 00:10:55 +0000

    fix: update site URL in astro.config.mjs to correct staging environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://staging.silaras.com',
+  site: 'https://staging.silaraskotim.com',
   integrations: [react()],
   image: {
     domains: []
