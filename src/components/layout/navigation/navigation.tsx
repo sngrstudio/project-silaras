2a290a99f46096482354d1df152e2411ff089445
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-06-16 14:56:59 +0000

    Add new assets and update navigation and layout components
    - Added new web app manifest icons: 192x192 and 512x512 PNG files.
    - Introduced a new SVG asset for "designed by" branding.
    - Added new logo PNG asset for the site.
    - Updated navigation component to include the new logo image.
    - Enhanced navigation layout to include a drawer button for mobile view.
    - Updated user layout to display the logo above authentication forms.
    - Modified head component to include various favicon links and apple touch icon.

--- src/components/layout/navigation/navigation.tsx
+++ src/components/layout/navigation/navigation.tsx
@@ -10,6 +10,7 @@
  */
 import type { FC, PropsWithChildren } from 'react'
 import type { Site } from './navigation.astro'
+import DrawerButtonRC from '../drawer/button'
 
 /**
  * Props interface for the NavigationRC component
@@ -64,10 +65,12 @@ interface NavigationRCProps extends PropsWithChildren {
 const NavigationRC: FC<NavigationRCProps> = ({ children, site }) => {
   return (
     <nav className='navbar border-base-300 border-b'>
-      <div className='xl:hidden'>{children}</div>
+      <div className='xl:hidden'>
+        <DrawerButtonRC />
+      </div>
       <div className='flex-1'>
-        <a className='btn btn-ghost text-lg' href='/'>
-          <span>{site.SITE_NAME}</span>
+        <a className='btn btn-ghost text-lg' href='/' title={site.SITE_NAME}>
+          {children}
         </a>
       </div>
     </nav>
